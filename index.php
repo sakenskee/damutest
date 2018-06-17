2562efeaec1c9af21e188dbd471fc0df6418cbdc
sakenskee <z.s.t@mail.ru>
2018-06-17 18:43:18 +0600

    home

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>Quick start. Local server-side application with UI</title>
 </head>
 <body>
-<div id="auth-data">OAuth 2.0 data from REQUEST: OLEG SAN
+<div id="auth-data">OAuth 2.0 data from REQUEST: HOMETEST
     <PRE>
     <?
         print_r($_REQUEST);
